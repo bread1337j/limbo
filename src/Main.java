80f873233b35f77c518d1023197c8e5e511b53d3
agaziiantc <agaziiantc60@stuy.edu>
2024-08-20 14:11:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,166 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+
+public class Main {
+    Window[] w = new Window[99];
+    JFrame fr[] = new JFrame[99];
+    int ID = 0;
+    Window MakeWindow(int x, int y, int sizex, int sizey) throws InterruptedException {
+        //AI types
+        //1 = tries to stay at range away as much as possible
+        //2 = will be running headfirst at the enemy and minimizing the range
+        //3 = will stay at range+1 away
+        //4 = will move randomly
+        //COLORS
+        //0 = black
+        //1 = red
+        //2 = blue
+        //3 = green
+        //4 = orange
+        //5 = invisible
+        ID += 1;
+        w[ID] = new Window();
+        fr[ID] = w[ID].MakeScreen();
+        w[ID].GoTo(fr[ID], x, y, 1);
+        w[ID].setSize(fr[ID], sizex, sizey);
+        w[ID].setID(ID);
+        return(w[ID]);
+
+    }
+
+    public void limbo(Point2D[] p) throws InterruptedException {
+        Point2D[] pclone = p.clone();
+
+        for(Window wd : w){
+            if(wd!=null) {
+                int rnd = new Random().nextInt(pclone.length-1) + 1;
+                //System.out.println(rnd);
+                wd.GoTo(fr[wd.ID], (int) pclone[rnd].getX(), (int) pclone[rnd].getY(), 20);
+                Point2D[] pcloneclone = new Point2D[pclone.length-1];
+                for(int i=0, k=0;i<pclone.length;i++){
+                    if(i!=rnd){
+                        pcloneclone[k]=pclone[i];
+                        k++;
+                    }
+                }
+                pclone = pcloneclone;
+            }
+        }
+    }
+
+    public void Worm(int x, int y) throws InterruptedException {
+        //w[1].GoTo(fr[1], x, y, 10);
+        w[1].InstantGoTo(fr[1], x, y);
+        for(Window wd : w){
+            if(wd!=null){
+                if(wd.ID!=1) {
+                    int Originx = fr[wd.ID].getX();
+                    int Originy = fr[wd.ID].getY();
+                    double dist = Math.sqrt(Math.pow(fr[wd.ID-1].getX() - fr[wd.ID].getX(), 2)
+                            +
+                            Math.pow(fr[wd.ID-1].getY() - fr[wd.ID].getY(), 2)
+                    );
+                    double vectx = (fr[wd.ID-1].getX() - fr[wd.ID].getX()) / dist;
+                    double vecty = (fr[wd.ID-1].getY() - fr[wd.ID].getY()) / dist;
+                    //System.out.println(vectx);
+                    wd.InstantGoTo(fr[wd.ID], Originx + (int)(vectx * (dist-40)), Originy + (int)(vecty * (dist-40)));
+                    //wd.GoTo(fr[wd.ID], Originx + (int)(vectx * (dist-40)), Originy + (int)(vecty * (dist-40)), 2);
+                    //TimeUnit.MILLISECONDS.sleep(200);
+                }
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Main trollface = new Main();
+
+
+        for(int i = 0; i<4; i++){
+            trollface.MakeWindow(140 * i + 700, 200, 100,100);
+        }
+        for(int i = 0; i<4; i++){
+            trollface.MakeWindow(140 * i + 700, 600, 100,100);
+        }
+        //for(int i = 0; i<4; i++){
+        //    trollface.MakeWindow(140 * i + 700, 1000, 100,100);
+        //}
+        //for(int i = 0; i<4; i++){
+        //   trollface.MakeWindow(140 * i + 700, 1400, 100,100);
+        //}
+        TimeUnit.MILLISECONDS.sleep(200);
+
+        Point2D[] p = new Point2D[9];
+        int pID = 0;
+        for(int i = 0; i<4; i++){
+            pID += 1;
+            int finalI = i;
+            p[pID] = new Point2D() {
+                @Override
+                public double getX() {
+                    return 140 * finalI + 700;
+                }
+
+                @Override
+                public double getY() {
+                    return 200;
+                }
+
+                @Override
+                public void setLocation(double x, double y) {
+
+                }
+            };
+        }
+
+        for(int i = 0; i<4; i++){
+            pID += 1;
+            int finalI = i;
+            p[pID] = new Point2D() {
+                @Override
+                public double getX() {
+                    return 140 * finalI + 700;
+                }
+
+                @Override
+                public double getY() {
+                    return 600;
+                }
+
+                @Override
+                public void setLocation(double x, double y) {
+
+                }
+            };
+        }
+
+
+        //System.out.println(p[1].getX());
+        Random rand = new Random();
+        //for(int i=0; i<4; i++){
+        //    trollface.limbo(p);
+        //    TimeUnit.MILLISECONDS.sleep(130);
+        //}
+
+        //for(int i=0; i<10; i++) {
+        //    trollface.Worm(rand.nextInt(1900), rand.nextInt(1000));
+        //    TimeUnit.MILLISECONDS.sleep(100);
+        //}
+
+        while(true){
+            trollface.Worm(MouseInfo.getPointerInfo().getLocation().x, MouseInfo.getPointerInfo().getLocation().y);
+            //TimeUnit.MILLISECONDS.sleep(1);
+        }
+
+        //for(int i=0; i<40; i++){
+        //    trollface.limbo(p);
+        //    TimeUnit.MILLISECONDS.sleep(130);
+        //}
+    }
+
+}
---
+++ src/Window.java
@@ -0,0 +1,60 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JComponent;
+
+public class Window extends Thread {
+    int ID;
+    JFrame MakeScreen(){
+        JFrame fr = new JFrame();
+        fr.setSize(600, 600);
+        fr.setDefaultCloseOperation(3);
+        fr.setVisible(true);
+
+        return(fr);
+    }
+    JPanel MakePanel(JFrame fr){
+        JPanel pn = new JPanel();
+        fr.add(pn);
+        return(pn);
+    }
+    public void ClearScreen(JFrame fr, JPanel pn) {
+        fr.getContentPane().removeAll();
+        pn.removeAll();
+    }
+    public void setID(int IDvar){
+        ID = IDvar;
+    }
+    public void setSize(JFrame fr, int x, int y){
+        fr.setSize(x, y);
+    }
+    public void InstantGoTo(JFrame fr, int x, int y){
+        fr.setLocation(x, y);
+    }
+    public void GoTo(JFrame fr, int x, int y, int time) throws InterruptedException {
+        //double dist = Math.sqrt(x * x + y * y);
+        int OriginX = fr.getX();
+        int OriginY = fr.getY();
+
+        double vectx = (x - OriginX) / time;
+        //System.out.println(vectx);
+        double vecty = (y - OriginY) / time;
+        for(int i=0; i<=time+3; i++) {
+
+            fr.setLocation(
+                    OriginX + (int)(vectx * i),
+                    OriginY + (int)(vecty * i)
+            );
+            //System.out.println(originx);
+            //System.out.println(vectx*(i / time));
+            //System.out.println(originx + (int)Math.round(vectx*(i / time)));
+            //System.out.println(originy + (int)Math.round(vecty*(i / time)));
+            //System.out.println(OriginX + (int)(vecty * (time + 3)));
+            //Thread.sleep(1);
+            TimeUnit.MILLISECONDS.sleep(1);
+        }
+        //System.out.println(y);
+        fr.setLocation(x, y);
+    }
+}
